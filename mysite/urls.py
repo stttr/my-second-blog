6d2b0c4db5ce338f409671361805b530b5c9e89b
stttr <qazxcvbnmsr@gmail.com>
2019-09-20 20:52:08 +0900

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
 ]
